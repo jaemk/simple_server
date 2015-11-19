8255b30b8ba810178d7da5a5ffe596c243495777
James <james@pi-nipple.(none)>
2015-11-19 17:01:27 -0500

    added router.js placeholder functions

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var http = require('http');
 var os = require('os');
+var url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log("Request receieved.");
+        var pathname = url.parse(request.url).pathname;
+        
+        console.log("Request for " + pathname + " receieved.");
+        
+        route(pathname)
+        
         response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("hello!");
+        response.write("hello!" + pathname );
         response.end();
     }
     var port = 5000;
     var ni = os.networkInterfaces();
     var ip = ni['eth0'][0]['address'] 
     http.createServer(onRequest).listen(port);//, '0.0.0.0');
-    console.log("Server has started."+port);
+    console.log("Server has started on port: "+port);
 }
 
 exports.start = start;
